26ca6bece5080d221b4a2d2f3b9bf70ea474fc90
gasper94 <ulimarmol94@gmail.com>
2020-04-01 15:17:18 -0700

    adding backend

---
+++ backend/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+  port: 8080,
+  mongoUri:
+    "mongodb+srv://test_user:clearlabs@cluster0-wlcbv.mongodb.net/test?retryWrites=true&w=majority",
+  jwtSecret: "Ulises_Martinez_Moran"
+};
+
+export default config;
