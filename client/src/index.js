0f081a1cec2eb47d54eccb97091f79801883bd36
gasper94 <ulimarmol94@gmail.com>
2020-04-03 11:09:20 -0700

    fixing build and adding webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+// import _ from "lodash";
+
+// function component() {
+//   const element = document.createElement("div");
+
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
